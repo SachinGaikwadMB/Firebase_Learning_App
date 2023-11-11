941c7f3a5b42541cb19ac686f6e4891681986c98
sachin-gaikwad <sachin.gaikwad@mindbowser.com>
2023-11-11 12:30:17 +0530

    initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FIREBASE_DEMO';
+export class AppComponent implements OnInit{
+
+   products$ !:Observable<any>;
+  //  subscription !: Subscription;
+  //  products : any [] = [];
+  // jsonObj = {
+  //   key : 'value'
+  // };
+
+  constructor (private _af : AngularFireDatabase) {
+
+  }
+  
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+
+    getAll() {
+       this.products$ = this._af.list('/products').valueChanges();
+    }
+
+
+    // ngOnDestroy(): void {
+    //   this.subscription.unsubscribe();
+    // }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFireModule } from '@angular/fire/compat';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    AngularFireModule.initializeApp(environment.firebase),
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
